272cafba5ec815d81febc8484656c5bdd419a10a
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-01-06 00:21:24 -0700

    Initial commit

---
+++ File_Ecosystems/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  File_Ecosystems
+//
+//  Created by Emiliano Chavez De La Torre on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ File_Ecosystems/File_EcosystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_EcosystemsApp.swift
+//  File_Ecosystems
+//
+//  Created by Emiliano Chavez De La Torre on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct File_EcosystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
